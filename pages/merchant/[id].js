12e22b3b600a3e0defcc9a3ddc3962ae6ec1fd31
ganiirsyadi-tiket <gani.irsyadi@tiket.com>
2022-05-13 18:41:24 +0700

    feat: create merchant product page

---
+++ pages/merchant/[id].js
@@ -0,0 +1,42 @@
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import Banner from "../../components/Banner";
+import Navbar from "../../components/Navbar";
+import ProductList from "../../components/ProductList";
+import { getAllProducts } from "../../models/product";
+
+export default function MerchantProduct() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getAllProducts().then((data) => {
+      setProducts(data);
+      setLoading(false);
+    });
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>EXIT COMPUTER MANGO TWO - Layanan dan Aplikasi Web</title>
+        <meta
+          name="description"
+          content="It is an e-commerce application that provides various computer products and accessories at competitive prices."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      <main className="mx-auto max-w-6xl pt-4">
+        <Banner title="Toko Jaya Abadi" />
+        {loading ? (
+          <div className="mx-auto h-96 flex justify-center items-center">
+            <progress className="progress w-56"></progress>
+          </div>
+        ) : (
+          <ProductList products={products} />
+        )}
+      </main>
+    </>
+  );
+}
