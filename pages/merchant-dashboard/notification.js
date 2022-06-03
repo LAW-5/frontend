e0c57b4e81d17c1fc56199ece5aa6a197e82e08a
ganiirsyadi <ganiilhamirsyadi@gmail.com>
2022-06-03 15:51:34 +0700

    feat: integ merchant notification

--- pages/merchant-dashboard/notification.js
+++ pages/merchant-dashboard/notification.js
@@ -1,10 +1,22 @@
 import Head from "next/head";
+import { useEffect, useState } from 'react';
 import Banner from "../../components/Banner";
-import MerchantDashboardDrawer from '../../components/MerchantDashboardDrawer';
-import NavbarMerchant from '../../components/NavbarMerchant';
-import NotificationCard from '../../components/NotificationCard';
+import MerchantDashboardDrawer from "../../components/MerchantDashboardDrawer";
+import NavbarMerchant from "../../components/NavbarMerchant";
+import NotificationCard from "../../components/NotificationCard";
+import { getAllNotification } from '../../models/notification';
 
 const NotificationMerchant = () => {
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    getAllNotification().then((res) => {
+      if (!_.isEmpty(res.data)) {
+        setNotifications(res.data);
+      }
+    });
+  }, []);
+
   return (
     <>
       <Head>
@@ -13,14 +25,11 @@ const NotificationMerchant = () => {
       <div className="absolute top-0 left-0 w-full">
         <NavbarMerchant />
         <main className="mx-auto max-w-6xl pt-4">
-          <Banner title="Merchant Dashboard" />            
+          <Banner title="Merchant Dashboard" />
           <h2 className="my-8 text-2xl font-bold text-center">Notification</h2>
-          <NotificationCard />
-          <NotificationCard />
-          <NotificationCard />
-          <NotificationCard />
-          <NotificationCard />
-          <NotificationCard />
+          {notifications.map((notification) => (
+            <NotificationCard key={notification.id} {...notification} />
+          ))}
         </main>
       </div>
       <MerchantDashboardDrawer />
